64594a251a27898470b865ae89fe01fd0f100763
Marvin <32267352+dermrvn-code@users.noreply.github.com>
2024-06-15 21:19:28 +0200

    Reorganize Implement basic MASA Server Generate CA and CRTs for MASA

---
+++ MASA/masa.py
@@ -0,0 +1,63 @@
+import ssl
+import json
+import base64
+
+
+import sys
+sys.path.append("../") 
+from Voucher.voucher import Voucher, create_voucher, load_private_key
+
+from cryptography.x509 import load_pem_x509_certificate
+
+import http.server
+
+class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_POST(self):
+        if self.path == "/request-voucher":
+            # Validate client certificate here
+            # Add your validation logic here
+            # For example, you can check the client"s certificate against a known list of trusted certificates
+            
+            # Extract the POST request payload
+            content_length = int(self.headers["Content-Length"])
+            post_data = self.rfile.read(content_length)
+            post_data_dict = json.loads(post_data)
+            
+            # Extract the client"s certificate
+            client_cert_bytes = self.request.getpeercert(True)
+            client_cert_json = self.request.getpeercert()
+
+            registrar_domain = post_data_dict["domain"]
+            serial_number = post_data_dict["serialnumber"]
+            
+            print("Client certificate: ", json.dumps(client_cert_json))
+            print("POST request payload: ", json.dumps(post_data_dict))
+
+            private_key = load_private_key("certs/MASA_priv.key")
+            voucher = create_voucher(private_key, client_cert_bytes, registrar_domain, "verified", serial_number)
+            voucher_json = json.dumps(voucher.to_dict());
+
+            # Send response
+            self.send_response(200)
+            self.send_header("Content-type", "text/json")
+            self.end_headers()
+            self.wfile.write(str.encode(voucher_json))
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"Page not found")
+
+
+server_address = ("localhost", 8888)  # Change the port number if needed
+httpd = http.server.HTTPServer(server_address, MyHTTPRequestHandler)
+
+# Enable HTTPS by providing the path to your SSL certificate and key files
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context.verify_mode = ssl.CERT_REQUIRED
+context.load_cert_chain(certfile="./certs/MASA.crt", keyfile="./certs/MASA_priv.key")
+context.load_verify_locations(cafile="../Registrar/ca/registrar_CA.pem")
+httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+
+print("Server running on port https://localhost:8888 ...")
+httpd.serve_forever()
---
+++ MASA/test_masa.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+
+
+import sys
+sys.path.append("../") 
+from Voucher.voucher import Voucher, parse_voucher, load_certificate, load_public_key
+
+data = {
+    "serialnumber" : "8dhewfioiowefojpwef",
+    "domain" : "example.com"
+}
+
+response  = requests.post(
+    'https://localhost:8888/request-voucher', 
+    data=json.dumps(data), 
+    verify=False, 
+    cert=('../Registrar/certs/registrar.crt', '../Registrar/certs/registrar_priv.key')
+)
+voucher_json = response.content
+
+
+voucher = parse_voucher(voucher_json)
+print(json.dumps(voucher.to_dict(), indent=4))
+
+masa_pubkey = load_public_key("../MASA/certs/MASA_pub.key")
+registrar_cert = load_certificate("../Registrar/certs/registrar.crt")
+
+
+
+if(voucher.verify(masa_pubkey,"example.com", registrar_cert)):
+    print("Voucher is valid")
+else:
+    print("Voucher is invalid")
